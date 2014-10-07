6201688632b533aaa9b36b4150d0854a1fe20180
stephhippo <stephanie.hippo@gmail.com>
2014-10-07 01:55:26 -0400

    Prints single alignment for now.

--- alignment.py
+++ alignment.py
@@ -9,7 +9,8 @@ class Alignment:
         self.seq1 = seq1
         self.seq2 = seq2
         self.alignment_matrix = numpy.zeros((len(seq1)+1,len(seq2)+1))
-        self.trace_back_matrix = {}
+        self.trace_back_matrix = numpy.zeros((len(seq1)+1,len(seq2)+1))
+        #self.trace_back_matrix = {}
         self.optimal_solution_count = 0
 
     def initialize_alignment_matrices(self):
@@ -20,6 +21,14 @@ class Alignment:
             i = i + 1
         for slot in self.alignment_matrix:
             self.alignment_matrix[j][0] = self.indel * j
+            j = j + 1
+        i = 0
+        j = 0
+        for slot in self.trace_back_matrix[0]:
+            self.trace_back_matrix[0][i] = self.indel * i
+            i = i + 1
+        for slot in self.trace_back_matrix:
+            self.trace_back_matrix[j][0] = self.indel * j
             j = j + 1
 
     def global_align(self):
@@ -40,25 +49,49 @@ class Alignment:
                         diagonal = self.alignment_matrix[i-1][j-1] + score
                         optimal_choice = max([left, upper, diagonal])
                         self.alignment_matrix[i][j] = optimal_choice
-                        self.trace_back_matrix[i,j]=[]
                         path = []
                         if optimal_choice == left:
-                            path.append("LEFT")
-                        if optimal_choice == upper:
-                            path.append("UPPER")
-                        if optimal_choice == diagonal:
-                            path.append("DIAGONAL")
-
-                        self.trace_back_matrix[i,j] = path
+                            self.trace_back_matrix[i,j] = -1
+                        elif optimal_choice == upper:
+                            self.trace_back_matrix[i,j] = 1
+                        elif optimal_choice == diagonal:
+                            self.trace_back_matrix[i,j] = 0
                         print ""
                     else:
                         print "Skipping first column"
             else:
                 print "Skipping first row"
 
+    def trace_back(self):
+        i = len(self.seq1)
+        j = len(self.seq2)
+        alnseq1=""
+        alnseq2=""
+        while i>0 or j>0:
+            if self.trace_back_matrix[i,j]==0:
+                alnseq1=alnseq1 + self.seq1[i-1]
+                alnseq2=alnseq2 + self.seq2[j-1]
+                i = i - 1
+                j = j - 1
+            elif self.trace_back_matrix[i,j]==1:
+                alnseq1=alnseq1 + self.seq1[i-1]
+                alnseq2=alnseq2 +"-"
+                i = i -1
+            elif self.trace_back_matrix[i,j]==-1:
+                alnseq1=alnseq1 + "-"
+                alnseq2=alnseq2 + self.seq2[j-1]
+                j = j-1
+        print alnseq1
+        print alnseq2
+
     def report_optimal_score(self):
+        print "Alignment Matrix"
         print self.alignment_matrix
+        print "Traceback Matrix"
+        print self.trace_back_matrix
+        print "Score"
         print self.alignment_matrix[len(self.seq1)][len(self.seq2)]
+        self.trace_back()
 
 
 
